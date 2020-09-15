975808396b123940d5d21cb0bbeda1b84a1b2658
Alexey <lesha.Alex9.denisov@mail.ru>
2020-09-15 19:58:30 +0300

    Added class "User"

---
+++ DAO/DAO/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    class User
+    {
+        public string name;
+        public string surname;
+        public int Id;
+        public User(int Id, string name, string surname)
+        {
+            this.Id = Id;
+            this.name = name;
+            this.surname = surname;
+        }
+    }
+}
